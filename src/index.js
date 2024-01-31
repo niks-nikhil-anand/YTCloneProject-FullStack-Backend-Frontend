b7a18d543ffd4c8a84d1d8227ffc609d10565853
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-01-31 12:26:14 +0530

    express configured

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ dotenv.config({
     path : "./env"
 })
 connectDB()
+.then(()=>{
+    app.on("error" , ()=>{
+        console.log("Error :" , error)
+    })
+    app.listen(process.env.PORT , ()=>{
+        console.log(`Server is running at Port : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("DB connection Failed" , err)
+})
