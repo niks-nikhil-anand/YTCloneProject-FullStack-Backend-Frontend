c788145f4e89b24ddaae33d2e8e3fa836887f8f6
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-02-01 09:52:25 +0530

    setup cloudinary - to upload locally saved file on cloud

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  });
+
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try {
+        if (!localFilePath) return null 
+        // upload the file on cloudinary 
+        const response = await cloudinary.uploader.upload(localFilePath ,{
+            resource_type : "auto" 
+        })  
+        // file is succesfully uploaded on cloudinary
+        return response;  
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved file as it 
+        // can be malicious file as it is failed to upload on cloudinary
+        return null ;
+    }
+}
+
+export {uploadOnCloudinary}         
