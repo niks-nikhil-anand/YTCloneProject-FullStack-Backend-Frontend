3b84c5af0a343ec8f6889ba006bde2a0081778bb
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-02-13 20:28:21 +0530

    Added healthcheck controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,2 +1,14 @@
 import mongoose from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler";
+import { apiResponse } from "../utils/apiResponse";
 
+
+const healthCheck =  asyncHandler(async(req,res) =>{
+    return res
+    .status(200)
+    .json(
+        new apiResponse(200 , {} , "Health of server is healthy")
+    )
+})
+
+export{ healthChech}
