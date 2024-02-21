6da73f3d85d91dfa87d5481037d8aa251f005076
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-02-21 13:54:22 +0530

    Added getPlaylistById controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -254,6 +254,120 @@ const deletePlaylist = asyncHandler(async(req,res) =>{
 })
 
 const updatePlaylist = asyncHandler(async(req, res) => {
+
+    // update Playlist
+    const {playlistId} = req.params
+
+    const {name , description} = req.body
+
+    if (!playlistId) {
+        throw new apiError(400 , "PlaylistId is required")
+    }
+
+    try {
+        //check user is the owner of the playlist or not ?
+    
+        const isOwner = isOwnerOfPlaylist(playlistId , req?.user?._id)
+    
+        if (!isOwner) {
+            throw new apiError(300 , "Unauthorized Access")
+        }
+
+        // now update playlist
+
+        const updatedPlaylist = await Playlist.findByIdAndUpdate(playlistId ,{
+            $set : {
+                name : name ,
+                description : description
+            }
+        })
+
+        if (!updatedPlaylist) {
+            throw new apiError(500 , "Something went wrong while updating the Playlist")
+        }
+
+        return res
+        .status(200)
+        .json(
+            new apiResponse(
+                200 ,
+                updatedPlaylist ,
+                "Playlist is updated successfully"
+            )
+        )
+    
+    } catch (error) {
+        throw new apiError(500 , error?.message || "Unable to update playlist")
+    }
+
+})
+
+const getPlaylistById = asyncHandler(async(req,res) =>{
+    const {playlistId} = req.params
+
+    if (!playlistId) {
+        throw new apiError(400 , "PlaylistId is required")
+    }
+
+    try {
+        const playlist = await Playlist.aggregate([
+            {
+                $match : {
+                    _id : new mongoose.Types.ObjectId(playlistId)
+                }
+            } ,
+            {
+                // if the user is the owner of the playlist , then he can also see unpublished video also , but if he is not the owner of the playlist he will only see the publsihed videos.
+
+                $project :{
+                    name : 1 ,
+                    description : 1 ,
+                    owner : 1 ,
+                    video : {
+                        $cond : {
+                            if :{
+                                $eq :["$owner" , new mongoose.Types.ObjectId(req?.user?._id)]
+                            } ,
+                            then : "$video" ,
+                            else: {
+                                $filter : {
+                                    input : "$video" ,
+                                    as : "video" ,
+                                    cond : {
+                                        $eq : ["$$video.isPublished" , true]
+                                    }
+                                }
+                            } 
+                        }
+                    } ,
+                    createdAt : 1 ,
+                    updatedAt : 1
+                }  
+            }
+            
+
+        ])
+
+        if (!playlist || playlist.length === 0) {
+            throw new apiError(404 , "Playlist not found ")
+        }
+
+        return res
+        .status(200)
+        .json(
+            new apiResponse(
+                200 , 
+                playlist ,
+                "Playlist is fetched succesfully"
+            )
+        )
+    } catch (error) {
+       throw new apiError(500 , error?.message || "Playlist id is incorrect") 
+    }
+
+})
+
+const getUserPlaylists = asyncHandler(async(req , res) => {
     
 })
 
@@ -262,7 +376,9 @@ export {
     addVideoToPlaylist,
     removeVideoFromPlaylist,
     deletePlaylist ,
-    updatePlaylist
+    updatePlaylist ,
+    getPlaylistById ,
+    getUserPlaylists
 
 
 }
