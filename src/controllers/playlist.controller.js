2ca10c33340267a5e785a93558ccc54f1d5f7f1c
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-02-21 12:15:22 +0530

    added deletePlaylist controller and updatePlaylist controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 import {asyncHandler} from '../utils/asyncHandler'
 import {apiResponse} from '../utils/apiResponse'
 import {apiError} from '../utils/apiError'
@@ -129,13 +129,140 @@ const addVideoToPlaylist = asyncHandler(async(req,res) =>{
 })
 
 const removeVideoFromPlaylist = asyncHandler(async(req,res) =>{
-    
+        const {playlistId , videoId} = req.params
+
+        if (!playlistId || !videoId) {
+            throw new apiError(400 , "PlaylistId and videoId is required")
+        }
+
+        try {
+            // check he is the owner of the playlist or not?
+
+            const isOwner = isOwnerOfPlaylist(playlistId , req?.user?._id)
+
+            if (!isOwner) {
+                throw new apiError(300, "Unauthorized access")
+            }
+
+
+            // now check video is available and published or not
+            const video = Video.findById(videoId)
+
+            if (!video) {
+               throw new apiError(404 , "Video not found") 
+            }
+
+            // check video is already added to the playlist or not
+
+            const playlist = await Playlist.findById(playlistId)
+
+            if (!playlist.video.includes(videoId)) {
+                throw new apiError(404 , "No video is available in the Playlist")
+            }
+
+            // Now remove the video from the Playlist
+
+            // if video is not published
+            if (!video?.isPublished) {
+                const removeVideo = await Playlist.updateOne({
+                    _id : Mongoose.Types.ObjectId(playlistId)
+                } ,
+                {
+                    $pull : {
+                        video : videoId
+                    }
+                })
+
+                if (!removeVideo) {
+                    throw new apiError(500 , "Unable to remove video from the playlist retry!!!" )
+                }
+
+                return res
+                .status(200)
+                .json(
+                    new apiResponse(
+                        200 , 
+                        {},
+                        "Video Not found in the playlist"
+                    )
+                )
+            }
+        const removeVideo = await Playlist.updateOne({
+            _id : Mongoose.Types.ObjectId(playlistId)
+        } ,
+        {
+            $pull : {
+                video : videoId
+            }
+        })
+
+        if (!removeVideo) {
+            throw new apiError(500 , "Unable to remove video from the playlist retry!!!")
+        }
+
+        return res
+        .status(200)
+        .json(
+            new apiResponse(
+                200 , 
+                removeVideo ,
+                "Video remove from the playlist successfully"
+            )
+        )
+
+        } catch (error) {
+            throw new apiError(500 , error?.message || "Unable to remove video from the playlist")
+        }
 })
 
+
+const deletePlaylist = asyncHandler(async(req,res) =>{
+    const {playlistId} = req.params
+
+    if (!playlistId) {
+        throw new apiError(400 , "PlaylistId is required")
+    }
+
+    try {
+        
+        // check user is the owner of the playlist or not 
+        const isOwner = isOwnerOfPlaylist(playlistId , req?.user?._id)
+
+        if (!isOwner) {
+            throw new apiError(300 , "Unauthorised access")
+        }
+
+        const deletedPlaylist = await Playlist.findByIdAndDelete(playlistId)
+
+        if (!deletedPlaylist) {
+            throw new apiError(500 , "Unable to delete the playlist")
+        }
+
+        return res
+        .status(200)
+        .json(
+            new apiResponse(
+                200 , 
+                deletedPlaylist ,
+                "Playlist is deleted successfully"
+            )
+        )
+
+    } catch (error) {
+        throw new apiError(500 , error?.message || "Unable to delete playlist")
+    }
+})
+
+const updatePlaylist = asyncHandler(async(req, res) => {
+    
+})
 
 export {
     createPlaylist ,
     addVideoToPlaylist,
-    removeVideoFromPlaylist
+    removeVideoFromPlaylist,
+    deletePlaylist ,
+    updatePlaylist
+
 
 }
